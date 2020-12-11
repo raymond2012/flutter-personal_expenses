36f76765c16e58afa80cb8cda9782fd42fd4157f
Raymond <raymond96521>
2020-12-11 15:37:08 +0800

    Use String Interpolation for amount

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class MyHomePage extends StatelessWidget {
                   decoration: BoxDecoration(
                       border: Border.all(color: Colors.purple, width: 2)),
                   child: Text(
-                    tx.amount.toString(),
+                    '\$${tx.amount}',
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
