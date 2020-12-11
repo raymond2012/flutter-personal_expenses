90124a714076f5a3f2b76ce93a242c3091dfd711
Raymond <raymond96521>
2020-12-11 14:44:46 +0800

    Create the data model

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +14,11 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()
+        id: 't2', title: 'Weekly Croceries', amount: 16.53, date: DateTime.now())
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
